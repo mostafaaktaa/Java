4ead18df0337951a2d19e6bcd9f6fa22f998ce1f
Mostafa Aktaa <42206469+mostafaaktaa@users.noreply.github.com>
2022-03-19 03:15:16 +0100

    axdsg

---
+++ javaUebung04/src/javaUebung04/Gruss.java
@@ -0,0 +1,10 @@
+package javaUebung04;
+
+public class Gruss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("asdf");
+	}
+
+}
---
+++ javaUebung04/src/module-info.java
@@ -0,0 +1,2 @@
+module javaUebung04 {
+}
